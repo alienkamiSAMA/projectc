668ab7eee95a26f4d1ecad5917065d11989dfb88
alienkamiSAMA <115788512+alienkamiSAMA@users.noreply.github.com>
2022-11-22 21:57:48 +0100

    Update mainutilisateur.c

--- mainutilisateur.c
+++ mainutilisateur.c
@@ -4,81 +4,34 @@
 
 int main()
 {
-    utilisateur p;
-    int ucin1,ucin2,ucin3,ucin4,x,x1,x2,x3,x4,n,n1,n2;
-    do{
-    printf("votre choix:\3\n0:ajouter\n1:modifier\n2:supprimer\n3:afficher\n4:affecter\n ");
-    scanf("%d",&n);
-    do{
-    switch (n){
-    case 0:{
-    saisie (&p);
-
-    x=ajouter("point.txt",&p);
+    char filename;
+    utilisateur p1= {"ahmed","achahlaou",25,06,1999,2,1,1};
+    utilisateur p2= {"ahmed","achah",20,06,1929,6,1,0}; /*fama saisie fi les maquettes but how to create a link between them*/
+    int x;
+    x=ajouter(filename,p1);
 
     if(x==1)
-        printf("\najout d'electeur avec succes\n");
-    else
-        printf("\nechec ajout");
-    break;
-    }
-    case 1:{
-    printf("donner la cin d'etulisateur");
-    scanf("%d",&ucin1);
-    x1=modifier("electeur.txt",ucin1,p );
+        printf("\najout d'utlisateur avec succés");
+    else printf("\nechec ajout");
 
+    x=ajouter(filename,p2);
 
-    if(x1==1)
-        printf("\nModification d'electeur avec succes\n");
-    else
-        printf("\nechec Modification\n");
+    if(x==1)
+        printf("\najout d'utlisateur avec succés");
+    else printf("\nechec ajout");
 
-    printf("donner la cin d'etulisateur");
-    scanf("%d",&ucin2);
-    break;
-    }
-    case 2:{
-    x2=supprimer("electeur.txt",ucin2 );
-    if(x2==1)
-        printf("\nSuppression de point avec succés\n");
-    else
-    printf("\nechec Suppression\n");
+    x=modifier(filename,1,p2 );
 
+    if(x==1)
+        printf("\nModification d'ulisateur avec succés");
+    else printf("\nechec Modification");
+    x=supprimer(filename,2 );
+    if(x==1)
+        printf("\nSuppression d'ulisateur  avec succés");
+    else printf("\nechec Suppression");
+    x=afficher(filename,0 );
+    if(x==-1)
+        printf("\nintrouvable");
 
-    break;
-    }
-    case 3:{
-    printf("donner la cin d'utilisateur");
-    scanf("%d",&ucin3);
-    x3=afficher("electeur.txt",ucin3,p );
-    if(x3==-1)
-        printf("introuvable\n");
-    else
-        printf("trouvable\n");
-
-    break;
-    }
-    case 4:{
-    printf("donner la cin d'utilisateur");
-    scanf("%d",&ucin4);
-    x4=affecter("electeur.txt",ucin4,p);
-    if(x4==-1)
-        printf("introuvable\n");
-    else
-        printf("trouvable\n");
-
-
-
-        break;
-    }
-
-}
-printf("modification generale terminer?:0:Y/1:N\n");
-scanf("%d",&n1);
-}while(n1!=0);
-printf("choix terminer?:0:Y/1:N\n");
-scanf("%d",&n2);
-
-}while(n2!=0);
-return 0;
+       return 0;
 }
