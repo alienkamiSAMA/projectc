d0beaff958f5e610a5e27f4ff3746f21dacb4adc
alienkamiSAMA <115788512+alienkamiSAMA@users.noreply.github.com>
2022-11-22 22:10:20 +0100

    Update utilisateur.h

--- utilisateur.h
+++ utilisateur.h
@@ -13,20 +13,19 @@ typedef struct
     char nom[20];
     char prenom[20];
     date udate;
-    int bv;
     int ucin;
     int role;
     int sexe;
+    int bv;
 
 } utilisateur;
 
 
-void saisie (utilisateur *p);
-int ajouter(char *filename, utilisateur *p );
-int modifier( char *filename, int ucin1, utilisateur p );
-int supprimer(char *filename, int ucin2 );
-int afficher(char *filename, int ucin3,utilisateur p);
-int affecter (char *filename,int ucin4 ,utilisateur p);
+
+int ajouter(char *filename, utilisateur p );
+int modifier( char *filename, int id, utilisateur nov );
+int supprimer(char *filename, int id );
+int afficher(char *filename, int id);
+int affecter (char *filename,int id);
 
 #endif
-
