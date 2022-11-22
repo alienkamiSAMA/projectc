cccc91ee8c80a21c55712f5e3d39d87a4153a184
alienkamiSAMA <115788512+alienkamiSAMA@users.noreply.github.com>
2022-11-22 22:09:05 +0100

    Update utilisateur.c

--- utilisateur.c
+++ utilisateur.c
@@ -2,182 +2,105 @@
 #include <stdio.h>
 #include <string.h>
 
-void saisie (utilisateur *p)
-{
-    printf("donner le nom d'electeur ");
-    scanf("%s",(*p).nom);
-    printf("donner le prenom d'electeur ");
-    scanf("%s",(*p).prenom);
-    printf("donner la date de naissance d'electeur ");
-    scanf("%d%d%d",&(*p).udate.jour,&(*p).udate.mois,&(*p).udate.annee);
-    printf("donner le cin d'electeur ");
-    scanf("%d",&(*p).ucin);
-    printf("donner le role d'electeur:\n 0:electeur\n 1:agent du bureau\n 2:admin\n");
-    scanf("%d",&(*p).role);
-    printf("donner le sexe d'electeur:\n 0:femme\n 1:homme\n");
-    scanf("%d",&(*p).sexe);
 
-
-}
-
-int ajouter(char *filename, utilisateur *p )
+int ajouter(char *filename, utilisateur p )
 {
 
-    FILE * f=fopen(filename, "a");
+    FILE * f=fopen("filename.txt", "a");
     if(f!=NULL)
     {
-        fprintf(f,"nom:%s   prenom:%s   date de naissance:%d/%d/%d   cin:%d login:%d    mot de passe:%d     role:%d     sexe:%d\n",(*p).nom,(*p).prenom,(*p).udate.jour,(*p).udate.mois,(*p).udate.annee,(*p).ucin,(*p).ucin,(*p).ucin,(*p).role,(*p).sexe);
+        fprintf(f,"nom:%s   prenom:%s   date de naissance:%d/%d/%d   id:%d  login:%d    mot de passe:%d     role:%d     sexe:%d\n",p.nom,p.prenom,p.udate.jour,p.udate.mois,p.udate.annee,p.ucin,p.ucin,p.ucin,p.role,p.sexe);
         fclose(f);
 
         return 1;
     }
     else return 0;
 }
-int modifier( char *filename, int ucin1, utilisateur p )
+int modifier( char *filename, int id, utilisateur nouv )
 {
-    int tr=0;
-    int x,x1;
-    utilisateur p1;
-    if(p.ucin== ucin1){
-    tr=1;
-    do{
-    printf("\n0:nom\n1:prenom\n2:date de naissance\n3:cin\n4:role\n5:sexe\n");
-    scanf("%d",&x);
-    switch(x)
-     {
-
-            case 0:
-            {    printf("nom\n");
-                 scanf("%s",&p1.nom);
-                 break;
-
-            }
-            case 1:
-            {
-                printf("prenom\n");
-                 scanf("%s",&p1.prenom);
-                 break;
-            }
-            case 2:
-                {
-                printf("donner la date de naissance d'electeur ");
-                scanf("%d%d%d",&p1.udate.jour,&p1.udate.mois,&p1.udate.annee);
-                break;
-                }
-            case 3:
-                {
-                    printf("donner le cin d'electeur ");
-                    scanf("%d",&p1.ucin);
-                    break;
-                }
-            case 4:
-                {
-                     printf("donner le role d'electeur:\n 0:electeur\n 1:agent du bureau\n 2:admin\n");
-                     scanf("%d",&p1.role);
-                     break;
-                }
-            case 5:
-                {
-                    printf("donner le sexe d'electeur:\n 0:femme\n 1:homme\n");
-                    scanf("%d",&p1.sexe);
-                    break;
-                }
-            default:
-                printf("choix invalide");
-
-
-     }
-     printf("modification terminer?:0:Y/1:N\n");
-     scanf("%d",&x1);
-    }while(x1==1);
-
-
-    FILE * f=fopen(filename, "r");
-    FILE * f2=fopen("nouv.txt", "w");
-    if(f!=NULL && f2!=NULL)
+utilisateur p;
+    FILE * f=fopen("filename.txt", "r");
+    FILE * f2 =fopen("aux.txt", "w");
+    if(f==NULL || f2==NULL)
+return 0;
+else
     {
-        while(fscanf(f,"nom:%s   prenom:%s   date de naissance:%d/%d/%d   cin:%d login:%d    mot de passe:%d     role:%d     sexe:%d\n",p.nom,p.prenom,p.udate.jour,p.udate.mois,p.udate.annee,p.ucin,p.ucin,p.ucin,p.role,p.sexe)!=EOF)
-        { if (tr==1){
-
-                fprintf(f2,"nom:%s   prenom:%s   date de naissance:%d/%d/%d   cin:%d login:%d    mot de passe:%d     role:%d     sexe:%d\n",p1.nom,p1.prenom,p1.udate.jour,p1.udate.mois,p1.udate.annee,p1.ucin,p1.ucin,p1.ucin,p1.role,p1.sexe);
-                fclose(f);
-
-                   }
-
-         else{
-
-                fprintf(f2,"nom:%s   prenom:%s   date de naissance:%d/%d/%d   cin:%d login:%d    mot de passe:%d     role:%d     sexe:%d\n",p.nom,p.prenom,p.udate.jour,p.udate.mois,p.udate.annee,p.ucin,p.ucin,p.ucin,p.role,p.sexe);
-                fclose(f2);
-             }
-    }
-    }
-
-
-    remove(filename);
-    rename("nouv.txt", filename);
-    return tr;
-
-}
-}
-int supprimer(char * filename, int ucin2)
+while(fscanf(f,"nom:%s   prenom:%s   date de naissance:%d/%d/%d   id:%d login:%d    mot de passe:%d     role:%d     sexe:%d\n",p.nom,p.prenom,&p.udate.jour,&p.udate.mois,&p.udate.annee,&p.ucin,&p.ucin,&p.ucin,&p.role,&p.sexe)!=EOF)
 {
-    int tr=0;
+if(p.ucin!=id)
+        fprintf(f2,"nom:%s   prenom:%s   date de naissance:%d/%d/%d   id:%d login:%d    mot de passe:%d     role:%d     sexe:%d\n",p.nom,p.prenom,p.udate.jour,p.udate.mois,p.udate.annee,p.ucin,p.ucin,p.ucin,p.role,p.sexe);
+else
+
+ fprintf(f2,"nom:%s   prenom:%s   date de naissance:%d/%d/%d   id:%d login:%d    mot de passe:%d     role:%d     sexe:%d\n",nouv.nom,nouv.prenom,nouv.udate.jour,nouv.udate.mois,nouv.udate.annee,nouv.ucin,nouv.ucin,nouv.ucin,nouv.role,nouv.sexe);
+
+}
+        fclose(f);
+        fclose(f2);
+remove("filename.txt");
+rename("aux.txt","filename.txt");
+        return 1;
+    }
+
+}
+int supprimer(char *filename, int id )
+{
+utilisateur p;
+    int tr;
+    FILE * f=fopen("filename.txt", "r");
+    FILE * f2 =fopen("aux.txt", "w");
+    if(f==NULL || f2==NULL)
+    tr=0;
+else
+    {
+while(fscanf(f,"nom:%s   prenom:%s   date de naissance:%d/%d/%d   id:%d login:%d    mot de passe:%d     role:%d     sexe:%d\n",p.nom,p.prenom,&p.udate.jour,&p.udate.mois,&p.udate.annee,&p.ucin,&p.ucin,&p.ucin,&p.role,&p.sexe)!=EOF)
+{
+if(p.ucin!=id){
+        tr=1;
+        fprintf(f2,"nom:%s   prenom:%s   date de naissance:%d/%d/%d   id:%d login:%d    mot de passe:%d     role:%d     sexe:%d\n",p.nom,p.prenom,p.udate.jour,p.udate.mois,p.udate.annee,p.ucin,p.ucin,p.ucin,p.role,p.sexe);
+}
+else
+        tr=0;
+
+}
+        fclose(f);
+        fclose(f2);
+remove("filename.txt");
+rename("aux.txt", "filename.txt");
+
+
+        return tr;
+    }
+}
+
+int afficher(char *filename, int id)
+{
+utilisateur p; int tr=0;
+    FILE * f=fopen("filename.txt", "r");
+ if(f!=NULL )
+    {
+while(fscanf(f,"nom:%s   prenom:%s   date de naissance:%d/%d/%d   id:%d login:%d    mot de passe:%d     role:%d     sexe:%d\n",p.nom,p.prenom,&p.udate.jour,&p.udate.mois,&p.udate.annee,&p.ucin,&p.ucin,&p.ucin,&p.role,&p.sexe)!=EOF && tr==0)
+{if(id==p.ucin)
+tr=1;
+}
+}
+if(tr==0)
+p.ucin=-1;
+return p.ucin;
+
+}
+
+
+int affecter (char *filename,int id)
+{
     utilisateur p;
-
-
-    FILE * f=fopen(filename, "r");
-    FILE * f2=fopen("nouv.txt", "w");
-    if(f!=NULL && f2!=NULL)
-    {
-        while(fscanf(f,"nom:%s   prenom:%s   date de naissance:%d/%d/%d   cin:%d login:%d    mot de passe:%d     role:%d     sexe:%d\n",p.nom,p.prenom,p.udate.jour,p.udate.mois,p.udate.annee,p.ucin,p.ucin,p.ucin,p.role,p.sexe)!=EOF)
-        {
-            if(p.ucin== ucin2)
-                tr=1;
-
-
-           else
-                fprintf(f2,"nom:%s   prenom:%s   date de naissance:%d/%d/%d   cin:%d login:%d    mot de passe:%d     role:%d     sexe:%d\n",p.nom,p.prenom,p.udate.jour,p.udate.mois,p.udate.annee,p.ucin,p.ucin,p.ucin,p.role,p.sexe);
-        }
-    }
-    fclose(f);
-    fclose(f2);
-    remove(filename);
-    rename("electeur.txt", filename);
-    return tr;
-}
-int afficher(char *filename, int ucin3,utilisateur p)
-{
-
     int tr,x=0;
 
-    FILE * f=fopen(filename, "r");
+    FILE * f=fopen("filename.txt", "r");
     if(f!=NULL)
     {
         while(tr==0 && fscanf(f,"nom:%s   prenom:%s   date de naissance:%d/%d/%d   cin:%d login:%d    mot de passe:%d     role:%d     sexe:%d\n",p.nom,p.prenom,p.udate.jour,p.udate.mois,p.udate.annee,p.ucin,p.ucin,p.ucin,p.role,p.sexe)!=EOF)
         {
-              if(p.ucin== ucin3)
-                tr=1;
-
-        }
-    } fclose(f);
-
-
-    if(tr==0)
-        x=-1;
-    return x;
-
-}
-int affecter(char *filename, int ucin4,utilisateur p)
-{
-
-    int tr,x=0;
-
-    FILE * f=fopen(filename, "r");
-    if(f!=NULL)
-    {
-        while(tr==0 && fscanf(f,"nom:%s   prenom:%s   date de naissance:%d/%d/%d   cin:%d login:%d    mot de passe:%d     role:%d     sexe:%d\n",p.nom,p.prenom,p.udate.jour,p.udate.mois,p.udate.annee,p.ucin,p.ucin,p.ucin,p.role,p.sexe)!=EOF)
-        {
-              if(p.ucin== ucin4){
+              if(p.ucin== id){
                 printf("num de bureau de vote :");
                 scanf("%d",p.bv);
                 tr=1;
@@ -195,4 +118,3 @@ int affecter(char *filename, int ucin4,utilisateur p)
 
 
 }
-
